9ef159f94f95c915910d9367e8c65022b444a0e9
Nour Midan <58523548+NourMidan@users.noreply.github.com>
2020-10-19 09:40:18 +0200

    Updates

---
+++ precache-manifest.165e21736f5fbe11151ffb9796a56a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf423a97b035f8bf9a8f7747b258f542",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "a570a5b6b4e4f77cffc1",
+    "url": "/Todo-app/static/css/main.5c26a543.chunk.css"
+  },
+  {
+    "revision": "7915015e05d7399bf523",
+    "url": "/Todo-app/static/js/2.ed432db5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.ed432db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a570a5b6b4e4f77cffc1",
+    "url": "/Todo-app/static/js/main.674fbbe9.chunk.js"
+  },
+  {
+    "revision": "1eb6d6ec195e648776ea",
+    "url": "/Todo-app/static/js/runtime-main.375335ae.js"
+  }
+]);
